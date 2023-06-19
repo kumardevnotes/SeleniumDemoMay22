2152d903a790aa536ffb25201cbdce84d0dd9b54
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-06-19 20:28:55 +0530

    added

--- src/test/java/com/exception/handling/ExceptionHandlingDemo2.java
+++ src/test/java/com/exception/handling/ExceptionHandlingDemo2.java
@@ -5,24 +5,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
-public class ExceptionHandlingDemo {
+public class ExceptionHandlingDemo2 {
 
-	public static void main(String[] args) {
-		Properties props = new Properties();
-
-		try {
-			String empName = "John";
-			System.out.println(empName.toUpperCase());
-
-			int num1 = 100;
-			int num2 = 10;
-			System.out.println(num1 / num2); // 100/0
-			
-			
+	public static void main(String[] args) throws IOException   {
+	
+		
 			//Below statement gives you the complete path of the current project
 			String rootPath =  System.getProperty("user.dir");
 			System.out.println(rootPath);
+			
 			FileReader myFile = new FileReader(rootPath + "//src//test//resources//appData.properties");
+			
+			Properties props = new Properties();
+			
 			props.load(myFile);
 			System.out.println(props.getProperty("appBrowser")); //Edge
 			System.out.println(props.getProperty("appUrl"));
@@ -32,30 +27,8 @@ public class ExceptionHandlingDemo {
 			//Below statement will print null on the console as we dont have a property "nitesh" in the appData file
 			System.out.println(props.getProperty("nitesh"));
 			
-
-		} catch (NullPointerException e) {
-			System.out.println("Caught an exception or error: " + e);
-		}
-
-		catch (FileNotFoundException e) {
-			System.out.println("Caught an exception or error: " + e);
-		}
 		
-		catch (IOException e) {
-			System.out.println("Caught an exception or error: " + e);
-		}
 		
-		catch (ArithmeticException e) {
-			System.out.println("Caught an exception or error: " + e);
-		}
-		
-		finally {
-			System.out.println("I will run always");
-			//removing data from props
-			props.clear();
-		}
-		
-		System.out.println("Program completed");
 	}
 
 }
